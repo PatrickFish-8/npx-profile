19dc59f52704678db248056e31c31cd8f9c295c8
PatrickFish-8 <patrickfish10@icloud.com>
2025-01-06 22:08:24 -0500

    updates and audio progress

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ import Enquirer from 'enquirer'
 import open from 'open'
 import terminalImage from 'terminal-image';
 import got from 'got';
+import playSound from 'play-sound';
 
 
 const mountains = await got("https://github.com/PatrickFish-8/npx-profile/blob/master/assets/mountains.jpg?raw=true").buffer();
 
 clear();
+
 const data = {
     name: chalk.bold.green("@pfish"),
     github: chalk.hex('#787878')("https://github.com/PatrickFish-8"),
@@ -57,15 +59,36 @@ const options = {
             name: '| resume',
             value: async () => {
                 console.log("opening resume ...")
-                open("https://www.google.com");
+                open("https://docs.google.com/document/d/1ww4sUPmC81j8lw_XrHVBjQQ61HpWrnSYz5TosbV7zSI/edit?tab=t.0");
             }
         },
         {
-            name: '| beautiful',
+            name: '| beautiful view',
             value: async () => {
                 try {
                     console.log(await terminalImage.buffer(mountains, {width: '100%'}));
                     console.log('Isn\'t that a nice view?')
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        },
+        {
+            name: '| cat',
+            value: async () => {
+                try {
+                    open("https://www.youtube.com/watch?v=5nxY9rMaE50&ab_channel=Bara");
+                    console.log("what the hell is wrong with this cat??");
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        },
+        {
+            name: '| song',
+            value: async () => {
+                try {
+
                 } catch (err) {
                     console.log(err);
                 }
@@ -76,32 +99,27 @@ const options = {
 }
 
 function main() {
-    try {
-        inquirer.prompt(options).then(async answer => {
-            if (answer.actions == "- exit") {
-                return;
-            }
-            else {
-                console.log('-'.repeat(40))
-                await answer.actions();
-                console.log('-'.repeat(40))
-    
-                Enquirer.prompt({
-                    type: "toggle",
-                    name: "again",
-                    message: "exit?",
-                    default: false
-                }).then(answer => {
-                    if (answer.again == false) {
-                        console.log('running again');
-                        main();
-                    } 
-                });
-            }
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    inquirer.prompt(options).then(async answer => {
+        if (answer.actions == "- exit") {
+            return;
+        } 
+        else {
+            console.log('-'.repeat(40))
+            await answer.actions();
+            console.log('-'.repeat(40))
+
+            Enquirer.prompt({
+                type: "toggle",
+                name: "again",
+                message: "exit?",
+                default: false
+            }).then(answer => {
+                if (answer.again == false) {
+                    main();
+                }
+            });
+        }
+    });
 }
 
 
